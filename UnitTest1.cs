f0d64d83907f700ed6f51e1c7a0b36a5b9672751
Zaupower <56695737+Zaupower@users.noreply.github.com>
2023-01-06 09:34:13 +0000

    Add proj files

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Selectors
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
